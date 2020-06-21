81b2b6bc54f4d825212b50b84338a7dc04c5b0f5
New Dad <josh_panakkal3122@hotmail.com>
2020-06-20 22:35:29 -0400

    continued with javascript

--- script.js
+++ script.js
@@ -1,66 +1,45 @@
 var startButton = document.querySelector("#button");
 var timerDisplay = document.querySelector("#heading2");
-var formH4Tag = document.querySelector("#nxtQuestion");
-var trueBtn = document.querySelector("#answer1");
-var falseBtn = document.querySelector("#answer2");
-var labelA = document.querySelector("#choiceA1");
-var labelB = document.querySelector("#choiceB1");
-var labelC = document.querySelector("#choiceC1");
+var next = document.querySelector("#nxtQ");
+var quest = document.querySelector("#question");
+var ans1 = document.querySelector("#answer1");
+var ans2 = document.querySelector("#answer2");
+var ans3 = document.querySelector("#answer3");
+
 
 var index = 0;
 var score = 0;
+var questions = [{q:"A _____________ can store different values at different times.", a1:"variable", a2:"boolean", a3:"string"},
+                 {q:"Building a new string out of other strings is called _______________.", a2:"concatenating", a1:"declaring", a3:"arrays"},
+                 {q:"To find the length of a string, add ___________ to the end.", a2:".length", a3:".querySelector", a1:".charAt"}
+];
 
-var questions = [{question:"A _____________ can store different values at different times.", answer1:"variable", answer2:"boolean", answer3:"string"},
-                 {question:"Building a new string out of other strings is called _______________.", answer:"concatenating"},
-                 {question:"To find the length of a string, add ___________ to the end.", answer:".length"}
-                ];
-
-//Countdown timer
-startButton.addEventListener("click", function(){
-   startButton.style.display = "none";
-   var time = 90;
-   showQuestions();
-    console.log(time);
-    var quizTimer = setInterval(function() {
-        startButton = " ";
-        time--;
-        timerDisplay.textContent = "TIME... " + time;
-        
-        if(time === 0) {
-            clearInterval(quizTimer);
-        }
-    }, 1000);
-
+startButton.addEventListener("click", function(){   
+   var time = 90;       
+   var quizTimer = setInterval(function() {   
+   time--;
+   timerDisplay.textContent = "TIME... " + time;        
+   if(time === 0) {
+   clearInterval(quizTimer);
+   }
+   }, 1000);        
 })
 
-//Displays questions
+//Function to display question and answers
 function showQuestions(){
-    formH4Tag.textContent = questions[index].question;
-    labelA.textContent = questions[index].answer1;
-    labelB.textContent = questions[index].answer2;
-    labelC.textContent = questions[index].answer3;
+    quest.textContent = questions[index].q; 
+
+    ans1.textContent = questions[index].a1;
     
+    ans2.textContent = questions[index].a2;
+    
+    ans3.textContent = questions[index].a3;
+    
+    index++;    
 }
-//Need a way to show multiple choice buttons for each questions
-trueBtn.addEventListener("click", function(){
-    //when this button is clicked, alert Correct
-    console.log("true button");
 
-    if(questions[index].answer) {
-        alert('You are correct!');
-        score++;
-    }
-    else {
-        alert("You are incorrect");
-        score--;
-
-    }
-
-
-})
-falseBtn.addEventListener("click", function(){
-    //when this button is clicked, alert Incorrect
-    console.log("false button");
+next.addEventListener("click", function(){
+    showQuestions();
 })
 
 
