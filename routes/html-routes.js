bd3cc0a580ec6be0c6a84a19ad1c2225eadfc931
New Dad <josh_panakkal3122@hotmail.com>
2020-11-27 16:18:30 -0500

    create html route for questions.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,5 +7,9 @@ module.exports = function(app) {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
+    
+    app.get("/quiz", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/questions.html"))
+    })
 }
 
