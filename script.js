85ba34ba06f74eaf8f2fb213fce57329d8b18e61
New Dad <josh_panakkal3122@hotmail.com>
2020-12-03 11:12:39 -0500

    update code for alert button to work

--- script.js
+++ script.js
@@ -1,114 +1,165 @@
-//questions and answers object
+const startBtn = document.getElementById("startButton");
+const form = document.getElementById("form");
+const question = document.getElementById("question");
+const choices = document.getElementById("choices");
+const choice1 = document.getElementById("choice1");
+const choice2 = document.getElementById("choice2");
+const choice3 = document.getElementById("choice3");
+const choice4 = document.getElementById("choice4");
+const alert1 = document.getElementById("alert1");
+const alertText1 = document.getElementById("alertText1");
+const closeBtn1 = document.getElementById("closeBtn1");
+const gameOver = document.getElementById("gameOver");
+const score = document.getElementById("score");
+const timer = document.getElementById("timer");
 
-var questions = [{
-    q:"What can store different values at different times?",
-    opt1:"variable", 
-    opt2:"boolean", 
-    opt3:"string",
-    a:"1"
-},{
-    q:"Building a new string out of other strings is called what?", 
-    opt1:"declaring",
-    opt2:"concatenating", 
-    opt3:"arrays",
-    a:"2"
-},{
-    q:"To find the length of a string, what should you add to the end?", 
-    opt1:".charAt",
-    opt2:".querySelector", 
-    opt3:".length",
-    a:"3"
-},{
-    q:"What characters are use to add comments to Javascript?", 
-    opt1:"/*",
-    opt2:"//", 
-    opt3:"<!-- -->",
-    a:"2"
-},{
-    q:"What is a type of variable that can contain many values, and uses name:value pairs?", 
-    opt1:"objects",
-    opt2:"arrays", 
-    opt3:"functions",
-    a:"1"
+let amount;
+let category;
+let difficulty;
+let newArray;
+let i = 0;
+let counter = 0;
+let time;
 
-}];
+initialize();
+playQuiz();
 
-var startButton = document.querySelector("#button");
-var timerDisplay = document.querySelector("#heading2");
-var quest = document.querySelector("#question");
-var option1 = document.querySelector("#option1");
-var option2 = document.querySelector("#option2");
-var option3 = document.querySelector("#option3");
-var answerButtons = document.querySelector("#answerBtns");
-var container = document.querySelector(".container");
+function initialize() {
 
+    // Make certain elements hidden initially
+    gameOver.style.display = "none";
+    alert1.style.display = "none";
+        
+    const queryString = window.location.search;
 
-var time = 20;  
-var index = 0;
-var score = 0;
-var totQuestions = questions.length;
+    // URL SearchParams Constructor
+    const urlParams = new URLSearchParams(queryString);
 
-
-//hide buttons at start
-option1.style.display = "none";
-option2.style.display = "none";
-option3.style.display = "none";
-
-
-//timer
-startButton.addEventListener("click", function(){
-    startButton.style.display = "none";
-    option1.style.display = "";
-    option2.style.display = "";
-    option3.style.display = "";
-   
-    showQuestions();
-         
-    var quizTimer = setInterval(function() {   
-    time--;
-    timerDisplay.textContent = "TIME... " + time;        
-    if(time === 0) {
-    clearInterval(quizTimer);
-    }
-    }, 1000);        
-})
-
-//Function to display question and answers
-function showQuestions(){
-    quest.textContent = questions[index].q; 
-    option1.textContent = questions[index].opt1;    
-    option2.textContent = questions[index].opt2;    
-    option3.textContent = questions[index].opt3;    
-       
+    // Update global variables with url params
+    amount = urlParams.get('amount');
+    category = urlParams.get('category');
+    difficulty = urlParams.get('difficulty');
     
-}
-//console.log(currentQuestions);
-answerButtons.addEventListener("click", function(event){
-    if(event.target.value === questions[index].a) {
-        alert("CORRECT!")
-        score++;
-        }
-    else{
-        alert("WRONG!");   
-        score--;        
-    } 
-    index++; 
-    if(index > totQuestions - 1){
-     gameOver();
+    // Set Timer
+    if(amount <= 8) {
+        time = 60;
     }
-    showQuestions();
-    //currentQuestions++;      
-})
+    else if (amount >= 9 && amount <= 11) {
+        time = 90;
+    }
+    else {
+        time = 180;
+    }
+    // Make Ajax call to API
+    getQuestions();
+}
 
-//game over function
-function gameOver (){
-    container.innerHTML = " ";
-    var tag = document.createElement("h1");
-    tag.textContent = "Your time is " + time;
-    container.appendChild(tag);
+//Ajax GET request to Open Trivia Database
+function getQuestions() {    
+    $.ajax({
+        url:"https://opentdb.com/api.php?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty + "&type=multiple",
+        method: "GET"
+    }).then(response => {
+        let array = response.results;
+        newArray = array.map(trivia => {
+            let answers = [trivia.incorrect_answers[0], trivia.incorrect_answers[1], trivia.incorrect_answers[2], trivia.correct_answer];
+            return {
+                category: trivia.category,
+                question: trivia.question,
+                choices: shuffle(answers),
+                answer: trivia.correct_answer
+            } 
+        }); 
+        
+        render(newArray);
+    });
+}
+
+// Fisher-Yates Shuffle Algorithm
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+// Render question and choices to html
+function render(array) {
+
+    score.textContent = counter;
+
+    // Replace special characters before rendering
+    question.textContent = array[i].question.replace(/&#039;/g, "'").replace(/&quot;/ig, '"');
+    choice1.textContent = array[i].choices[0].replace(/&#039;/g, "'").replace(/&quot;/ig, '"');
+    choice2.textContent = array[i].choices[1].replace(/&#039;/g, "'").replace(/&quot;/ig, '"');
+    choice3.textContent = array[i].choices[2].replace(/&#039;/g, "'").replace(/&quot;/ig, '"');
+    choice4.textContent = array[i].choices[3].replace(/&#039;/g, "'").replace(/&quot;/ig, '"');
+}
+
+// Play Quiz
+function playQuiz() {    
+    let quizTimer = setInterval(function() {   
+        time--;
+        timer.textContent = time;        
+            if(time === 0 || i > amount - 1) {
+                clearInterval(quizTimer);
+                gameCheck();
+            }
+    }, 1000);
+   
+    // Check answers using event delegation
+    choices.addEventListener("click", function(event) {
+        if(event.target.textContent === newArray[i].answer) {
+            alert1.style.display = "";
+            alert1.className = "alert1";            
+            alertText1.textContent = "CORRECT!"; 
+            counter++;              
+        }         
+        else {
+            alert1.style.display = "";
+            alert1.className = "alert2";
+            alertText1.textContent = "Incorrect - " + `${newArray[i].answer.toUpperCase().replace(/&#039;/g, "'").replace(/&quot;/ig, '"')}`;
+        }       
+    }); 
+    
+    // Close alert and go to next question or end quiz
+    closeBtn1.addEventListener("click", function() {
+        i++;                             
+        alert1.style.display = "none";  
+        gameCheck();                        
+    });         
+}
+
+// End Quiz
+function gameCheck() {
+    if(i > amount - 1 || time === 0) {
+        question.style.display = "none";
+        choice1.style.display = "none";
+        choice2.style.display = "none";
+        choice3.style.display = "none";
+        choice4.style.display = "none";
+        gameOver.style.display = "";
+    }
+    else {
+        render(newArray);         
+    }  
 }
 
 
 
 
 
+  
+  
